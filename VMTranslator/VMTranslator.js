3ae6da2c34252fba201080be6c6ad282c831150e
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-12-16 13:00:26 +0800

    Add basic function and return translation

--- VMTranslator/VMTranslator.js
+++ VMTranslator/VMTranslator.js
@@ -122,7 +122,7 @@ function push_to_stack(vm_line) {
     const [, mem_segment_vm, address] = vm_line.split(' ');
     const mem_segment = mem_segment_asm[mem_segment_vm];
     // increment SP, add segment_vm[address] to top of stack
-    return `//${vm_line}` +
+    return `// ${vm_line}` +
         `\n@${address}` +
         '\nD=A' +
         `\n@${mem_segment}` +
@@ -299,12 +299,109 @@ function handle_mem_access(vm_line) {
     }
     return asm;
 }
+function handle_branching(vm_line) {
+    console.log('branching: ', vm_line);
+    return vm_line;
+}
 
+function translate_function(vm_line, function_name) {
+    return `// ${vm_line}` +
+    `\n(${function_name})` + // initialize local variables
+    '\n@SP' +
+    '\nA=M' +
+    '\nM=0' +
+    '\n@SP' +
+    '\nM=M+1' +
+    '\n@SP' +
+    '\nA=M' +
+    '\nM=0' +
+    '\n@SP' +
+    '\nM=M+1\n';
+}
+
+function translate_return(vm_line) {
+    return `// ${vm_line}` +
+    '\n@LCL' +
+    '\nD=M' +
+    '\n@FRAME' +
+    '\nM=D' +
+    '\n@5' +
+    '\nD=D-A' +
+    '\nA=D' +
+    '\nD=M' +
+    '\n@RET' + // return address (frame - 5)
+    '\nM=D' +
+    '\n@SP' +
+    '\nM=M-1' +
+    '\nA=M' +
+    '\nD=M' +
+    '\n@ARG' +
+    '\nA=M' +
+    '\nM=D' +
+    '\n@ARG' +
+    '\nD=M+1' +
+    '\n@SP' +
+    '\nM=D' +
+    '\n@FRAME' +
+    '\nD=M' +
+    '\n@1' +
+    '\nD=D-A' +
+    '\nA=D' +
+    '\nD=M' +
+    '\n@THAT' + // restore that of caller
+    '\nM=D' +
+    '\n@FRAME' +
+    '\nD=M' +
+    '\n@2' +
+    '\nD=D-A' +
+    '\nA=D' +
+    '\nD=M' +
+    '\n@THIS' + // restore this of caller
+    '\nM=D' +
+    '\n@FRAME' +
+    '\nD=M' +
+    '\n@3' +
+    '\nD=D-A' +
+    '\nA=D' +
+    '\nD=M' +
+    '\n@ARG' +
+    '\nM=D' +
+    '\n@FRAME' +
+    '\nD=M' +
+    '\n@4' +
+    '\nD=D-A' +
+    '\nA=D' +
+    '\nD=M' +
+    '\n@LCL' + // restore LCL of caller
+    '\nM=D' +
+    '\n@RET' +
+    '\nA=M' +
+    '\n0;JMP\n'; // GOTO return address
+}
+
+function handle_function(vm_line) {
+    const [, function_name, ] = vm_line.split(' ');
+    let asm;
+    if (vm_line.includes('function')) {
+        asm = translate_function(vm_line, function_name);
+    } else if (vm_line.includes('return')) {
+        asm = translate_return(vm_line);
+    }
+    return asm;
+}
 function translate_vm_to_asm(vm_line) {
     let asm;
-    let is_logic_command = vm_line.split(' ').length === 1;
+    const commands = vm_line.split(' ');
+    const is_logic_command = commands.length === 1;
+    const is_branching_command = commands.length === 2;
+    const function_commands = ['function', 'call', 'return'];
+    const is_function_command = function_commands.some((function_command) => commands.includes(function_command));
 
-    if (is_logic_command) {
+    if (is_function_command) {
+        asm = handle_function(vm_line);
+    } else if (is_branching_command) {
+        asm = handle_branching(vm_line);
+    } else if (is_logic_command) {
         asm = handle_logic(vm_line);
     } else {
         asm = handle_mem_access(vm_line);
